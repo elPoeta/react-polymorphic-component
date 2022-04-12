944756114324571b7ae47843eb98ed98a9862887
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2022-04-12 08:23:18 -0300

    refactor to make Text comopenent polymorphic

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -2,14 +2,20 @@ import React from 'react'
 
 type Color = 'black' | 'white' | 'red' | 'blue' | 'yellow' | 'green' | 'purple' | 'orange';
 
-type TextProps<C extends React.ElementType> = {
+type AsProp<C extends React.ElementType> = {
   as?: C;
+}
+
+type TextProps = {
   color?: Color | 'black';
 };
 
-type Props<C extends React.ElementType> = React.PropsWithChildren<TextProps<C>> & Omit<React.ComponentPropsWithoutRef<C>, keyof TextProps<C>>
+type PropsToOmit<C extends React.ElementType, Props> = keyof (AsProp<C> & Props);
 
-const Text = <C extends React.ElementType = 'span'>({ as, color, style, children, ...rest }: Props<C>) => {
+type PolymorphicComponentProps<C extends React.ElementType, Props = {}> = React.PropsWithChildren<Props & AsProp<C>>
+  & Omit<React.ComponentPropsWithoutRef<C>, PropsToOmit<C, Props>>;
+
+const Text = <C extends React.ElementType = 'span'>({ as, color, style, children, ...rest }: PolymorphicComponentProps<C, TextProps>) => {
   const Component = as || 'span';
   const customStyle: React.StyleHTMLAttributes<unknown> = { style } || { style: {} };
   const composeStyle: React.StyleHTMLAttributes<unknown> = color ? { style: { ...customStyle.style, color } } : customStyle;
